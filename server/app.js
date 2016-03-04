a17661a98be0c648705af3b22dd63be6e8190936
marcosocon <oconmarcos97@gmail.com>
2016-03-04 18:12:03 -0300

    added sport name endpoint

--- server/app.js
+++ server/app.js
@@ -12,10 +12,30 @@ app.use(express.static(__dirname + '/../client'));
 app.get('/sports', (request, response) => {
   let sports = mongoUtil.sports();
   sports.find().toArray((error,docs) => {
-    console.log(JSON.stringify(docs));
     let sportNames = docs.map((sport) => sport.name);
     response.json( sportNames );
   });
-})
+});
+
+app.get("/sports/:name", (request, response) => {
+  let sportName = request.params.name;
+  console.log( "Sport name: ", sportName );
+
+  let sport = {
+    "name": "Cycling",
+    "goldMedals": [{
+      "division": "Men's Sprint",
+      "country": "UK",
+      "year": 2012
+    }, {
+      "division": "Women's Sprint",
+      "country": "Australia",
+      "year": 2012
+    }]
+  };
+
+  response.json(sport);
+});
+
 
 app.listen(8181, () => console.log("Listening on 8181"));
